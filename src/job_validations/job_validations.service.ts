9ee917eee69aee7590519297c80154801d48afb7
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-05-31 16:41:21 -0300

    feat: add question improvements for chatgpt

--- src/job_validations/job_validations.service.ts
+++ src/job_validations/job_validations.service.ts
@@ -25,15 +25,19 @@ export class JobValidationsService {
     3 - Não precisar de mais de um ano de experiência
     4 - Não precisar de graduação
     5 - Não precisar de inglês avançado ou fluente
+    6 - Ser uma vaga relacionada a desenvolvimento de software
 
     E formate sua resposta em formato JSON de forma retornar um boolean se a vaga é compatível ou não e com as hard skills presentes no texto, exemplo de retorno desejado:
    
     {
+    "vaga_junior": true,
     "hard_skills_compativel": false,
     "nao_precisa_de_grande_experiencia": false,
     "nao_precisa_de_graduacao": false,
     "nao_precisa_de_ingles_avancado": false,
+    "vaga_relacionada_a_desenvolvimento_de_software": true,
     "hard_skills": ["PHP", "JavaScript"]
+    "motivo_das_resposta": "A vaga é Júnior e não precisa de inglês avançado, mas precisa de PHP e JavaScript"
     }
    
 
@@ -45,13 +49,18 @@ export class JobValidationsService {
   }
 
   private formatResponse(responseMessage: string) {
+    console.log(responseMessage);
     const response = JSON.parse(responseMessage);
 
     return {
+      job_junior: response.vaga_junior,
       hard_skills_compatible: response.hard_skills_compativel,
       low_experience: response.nao_precisa_de_grande_experiencia,
       without_graduation: response.nao_precisa_de_graduacao,
       low_english_level: response.nao_precisa_de_ingles_avancado,
+      job_related_to_software_development:
+        response.vaga_relacionada_a_desenvolvimento_de_software,
+      reason_for_answer: response.motivo_das_resposta,
       job_approved:
         response.hard_skills_compativel &&
         response.nao_precisa_de_grande_experiencia &&
